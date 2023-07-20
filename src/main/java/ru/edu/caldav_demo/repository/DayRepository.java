f070bbddef57380b194a8094ce05d9e324fc680e
dmitrii sh <dmitrii@sh.com>
2023-07-21 00:22:01 +0300

    service

--- src/main/java/ru/edu/caldav_demo/repository/DayRepository.java
+++ src/main/java/ru/edu/caldav_demo/repository/DayRepository.java
@@ -1,10 +1,24 @@
 package ru.edu.caldav_demo.repository;
 
+import java.time.LocalDate;
+import java.util.LinkedList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.edu.caldav_demo.dict.CountryEnum;
 import ru.edu.caldav_demo.entity.Day;
 
 @Repository
 public interface DayRepository extends JpaRepository<Day, Long> {
+
+	@Query("""
+			select d from Day d
+			where d.countryCode = :country
+			and d.date between :startDate and :endDate
+			order by d.date
+			""")
+	LinkedList<Day> findAllBy(@Param("country") CountryEnum country, @Param("startDate") LocalDate startDate,
+			@Param("endDate") LocalDate endDate);
 
 }
--- src/main/java/ru/edu/caldav_demo/service/DayService.java
+++ src/main/java/ru/edu/caldav_demo/service/DayService.java
@@ -1,11 +1,21 @@
 package ru.edu.caldav_demo.service;
 
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.edu.caldav_demo.dict.CountryEnum;
+import ru.edu.caldav_demo.entity.Day;
 import ru.edu.caldav_demo.repository.DayRepository;
 
 @RequiredArgsConstructor
+@Service
 public class DayService {
 
 	private final DayRepository dayRepository;
+
+	public List<Day> getDayListBy(CountryEnum country, LocalDate startDate, LocalDate endDate) {
+		return dayRepository.findAllBy(country, startDate, endDate);
+	}
 
 }
