c045dba3efd9f691ceea0d4420e1f9a2ca7aa77c
dmitrii sh <dmitrii@sh.com>
2023-07-20 23:28:00 +0300

    init

---
+++ src/main/java/ru/edu/caldav_demo/CaldavDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.caldav_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaldavDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaldavDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/edu/caldav_demo/CaldavDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.caldav_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaldavDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
